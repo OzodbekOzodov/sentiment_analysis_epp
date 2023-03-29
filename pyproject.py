8e738371d5fb16ebd207d13b66f8ee98b4948d59
Ozodbek Ozodov <oo@candidate-select.com>
2023-03-29 14:09:40 +0200

    updated the pyproject.py and environment.yml files, made small adjustments

--- pyproject.py
+++ pyproject.py
@@ -5,20 +5,90 @@ import numpy as np
 from scipy.stats import norm
 import os
 import nltk
-from pytask import task
+import pytask
 
-@task
-def load_data():
+@pytask.mark.depends_on("data.csv")
+@pytask.mark.produces("data_pickle.pkl")
+def task_load_data(depends_on, produces):
     """
     Load the data from "src/data_management/data.csv" and return it as a pandas DataFrame.
     """
-    data = pd.read_csv("src/data_management/data.csv")
-    return data
+    data = pd.read_csv("src/data_management/data.csv", encoding="ISO-8859-1")
 
-data = load_data()
+    data = _clean_text(data)
+    data = _bag_of_words(data)
 
-@task(requires=[load_data])
-def create_sentiment_plot(data):
+
+
+def _clean_text(data):
+    """
+    Preprocess a DataFrame column of text strings for sentiment analysis by converting them to lowercase, removing
+    punctuation marks, and replacing periods and question marks with special tokens.
+
+    Parameters:
+        column (pandas.Series): The DataFrame column containing the text strings to preprocess.
+
+    Returns:
+        pandas.Series: The preprocessed text strings with special tokens for sentence segmentation.
+    """
+    # Convert the text to lowercase
+    data['Text'] = data['Text'].str.lower()
+
+    # Replace punctuation marks with special tokens
+    data['Text'] = data['Text'].str.translate(str.maketrans('', '', string.punctuation))
+    data['Text'] = data['Text'].str.replace(".", " <PERIOD> ")
+    data['Text'] = data['Text'].str.replace("?", " <QUESTION> ")
+    data.to_pickle(src/data_management/"data_pickle.pcl")
+    
+
+
+# data['soft_clean_text'] = clean_text_soft(data['Text'])
+
+@pytask.mark.depends_on(
+                        {task_load_data,
+                        _clean_text}
+                        )
+@pytask.mark.produces()
+def _bag_of_words(data):
+    """
+    Preprocess a text string for sentiment analysis by converting it to lowercase, removing punctuation marks
+    and stop words, and tokenizing it into individual words.
+
+    Parameters:
+        text (str or pandas.Series): The text string or pandas Series to preprocess.
+
+    Returns:
+        str or pandas.Series: A preprocessed string or a pandas Series with preprocessed text.
+    """
+    if isinstance(data, str):
+        # If the input is a string, preprocess it and return a string of tokens
+        # Convert the text to lowercase
+        data_pickle = text.lower()
+
+        # Remove punctuation marks
+        text = ''.join([c for c in text if c not in punctuation])
+
+        # Remove stop words
+        stop_words = set(stopwords.words('english'))
+        word_tokens = word_tokenize(text)
+        filtered_text = [w for w in word_tokens if not w in stop_words]
+
+        # Return the preprocessed text as a string of tokens
+        return ' '.join(filtered_text)
+    elif isinstance(text, pd.Series):
+        # If the input is a pandas Series, preprocess each element of the Series and return a new Series
+        return text.apply(bag_of_words)
+    else:
+        # Raise an error if the input is not a string or a pandas Series
+        raise ValueError("Input must be a string or a pandas Series")
+    
+
+
+
+
+@pytask.mark.depends_on(task_load_data, produces='data_pickle.pkl"')
+@pytask.mark.produces("graph_1_sentiment_counts.png")
+def create_sentiment_plot(data, produces):
     """Create a bar plot of the sentiment counts in the data and save it as a PNG image.
 
     Args:
@@ -48,63 +118,4 @@ def create_sentiment_plot(data):
         os.remove(file_name)
     plt.savefig(file_name)
 
-@task(requires=[load_data])
-def clean_text_soft(column):
-    """
-    Preprocess a DataFrame column of text strings for sentiment analysis by converting them to lowercase, removing
-    punctuation marks, and replacing periods and question marks with special tokens.
 
-    Parameters:
-        column (pandas.Series): The DataFrame column containing the text strings to preprocess.
-
-    Returns:
-        pandas.Series: The preprocessed text strings with special tokens for sentence segmentation.
-    """
-    # Convert the text to lowercase
-    column = column.str.lower()
-
-    # Replace punctuation marks with special tokens
-    column = column.str.translate(str.maketrans('', '', string.punctuation))
-    column = column.str.replace(".", " <PERIOD> ")
-    column = column.str.replace("?", " <QUESTION> ")
-
-    # Return the preprocessed text
-    return column
-
-
-data['soft_clean_text'] = clean_text_soft(data['Text'])
-
-@task(requires=[clean_text_soft])
-def bag_of_words(text):
-    """
-    Preprocess a text string for sentiment analysis by converting it to lowercase, removing punctuation marks
-    and stop words, and tokenizing it into individual words.
-
-    Parameters:
-        text (str or pandas.Series): The text string or pandas Series to preprocess.
-
-    Returns:
-        str or pandas.Series: A preprocessed string or a pandas Series with preprocessed text.
-    """
-    if isinstance(text, str):
-        # If the input is a string, preprocess it and return a string of tokens
-        # Convert the text to lowercase
-        text = text.lower()
-
-        # Remove punctuation marks
-        text = ''.join([c for c in text if c not in punctuation])
-
-        # Remove stop words
-        stop_words = set(stopwords.words('english'))
-        word_tokens = word_tokenize(text)
-        filtered_text = [w for w in word_tokens if not w in stop_words]
-
-        # Return the preprocessed text as a string of tokens
-        return ' '.join(filtered_text)
-    elif isinstance(text, pd.Series):
-        # If the input is a pandas Series, preprocess each element of the Series and return a new Series
-        return text.apply(bag_of_words)
-    else:
-        # Raise an error if the input is not a string or a pandas Series
-        raise ValueError("Input must be a string or a pandas Series")
-    
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. Sentiment Analysis in Python Using Kaggle Data documentation master file, created by
+   sphinx-quickstart on Wed Mar 29 11:15:23 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Sentiment Analysis in Python Using Kaggle Data's documentation!
+==========================================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
