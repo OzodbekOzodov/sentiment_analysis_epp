5b8e4a9fbcdb2c7bddb8dd53d2c2a0b19ecc7d74
OzodbekOzodov <80547523+OzodbekOzodov@users.noreply.github.com>
2023-03-29 21:32:33 +0200

    debugged task-related issues and achieved some of the tasks running

--- src/models/task_upload_clean.py
+++ src/models/task_upload_clean.py
@@ -8,9 +8,11 @@ import nltk
 import pytask
 from string import punctuation
 
+
+
 @pytask.mark.task
-@pytask.mark.depends_on("data.csv")
-@pytask.mark.produces("data_pickle.pkl")
+@pytask.mark.depends_on("src/data_management/data.csv")
+@pytask.mark.produces("src/output/data_pickle.pcl")
 def task_load_data(depends_on, produces):
     """
     Load the data from "src/data_management/data.csv" and return it as a pandas DataFrame.
@@ -19,7 +21,8 @@ def task_load_data(depends_on, produces):
 
     data = _clean_text(data)
     data = _bag_of_words(data)
-
+    produces = "src/outputs/data_pickle.pcl"
+    data.to_pickle(produces)
 
 import string
 def _clean_text(data):
@@ -43,6 +46,45 @@ def _clean_text(data):
     data.to_pickle(produces)
 
     
+import pytask
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+#from task_upload_clean import task_load_data
+
+@pytask.mark.task
+@pytask.mark.depends_on(task_load_data) #, produces='data_pickle.pkl"')
+@pytask.mark.produces("graph_1_sentiment_counts.png")
+def create_sentiment_plot(data, produces):
+    """Create a bar plot of the sentiment counts in the data and save it as a PNG image.
+
+    Args:
+        data (pandas.DataFrame): The data loaded from "src/data_management/data.csv".
+
+    Returns:
+        None
+    """
+    # create a pandas DataFrame with the sentiment counts
+    sentiment_counts = pd.DataFrame(data['Sentiment'].value_counts())
+
+    # create a bar plot of the sentiment counts
+    sentiment_counts.plot(kind='bar', legend=None)
+
+    # set the plot title and axis labels
+    plt.title('Sentiment Counts')
+    plt.xlabel('Sentiment')
+    plt.ylabel('Count')
+
+    # create the data_management directory if it doesn't exist
+    #if not os.path.exists('outputs'):
+    #    os.makedirs('outputs')
+
+    # save the plot in the data_management directory
+    file_name = "src/outputs/graph_1_sentiment_counts.png"
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    plt.savefig(file_name)
+
 
 
 # data['soft_clean_text'] = clean_text_soft(data['Text'])
@@ -52,11 +94,11 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from string import punctuation
 
-@pytask.mark.depends_on(
-                        {task_load_data,
-                        _clean_text}
-                        )
-@pytask.mark.produces()
+#@pytask.mark.depends_on(
+#                       {task_load_data,
+#                        _clean_text}
+#                        )
+#@pytask.mark.produces()
 def _bag_of_words(data):
     """
     Preprocess a text string for sentiment analysis by converting it to lowercase, removing punctuation marks
