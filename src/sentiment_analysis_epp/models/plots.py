92034c1d1281e28722907444992cf08d005ca471
OzodbekOzodov <80547523+OzodbekOzodov@users.noreply.github.com>
2023-03-31 15:13:27 +0200

    all plots and tasks are working properly in this setting, .tex file needs to be updated and compiled

--- src/sentiment_analysis_epp/models/plots.py
+++ src/sentiment_analysis_epp/models/plots.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import sys
 
 
-def plot_sentiment_hist(df, save_path=None):
+def plot_sentiment_bar(df, save_path=None):
     # Count the number of sentiments for each class
     sentiment_counts = df["Sentiment"].value_counts()
 
@@ -23,12 +23,9 @@ def plot_sentiment_hist(df, save_path=None):
         fig.savefig(save_path, dpi=300, bbox_inches="tight")
 
 
-import os
-import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
 
 def plot_performance(input_file, output_file):
     df = pd.read_csv(input_file)
@@ -47,15 +44,18 @@ def plot_performance(input_file, output_file):
     r3 = [x + barWidth for x in r2]
     r4 = [x + barWidth for x in r3]
 
-    plt.bar(r1, accuracy, color='royalblue', width=barWidth, edgecolor='white', label='Accuracy')
-    plt.bar(r2, precision, color='lightsteelblue', width=barWidth, edgecolor='white', label='Precision')
-    plt.bar(r3, recall, color='cornflowerblue', width=barWidth, edgecolor='white', label='Recall')
-    plt.bar(r4, f1_score, color='steelblue', width=barWidth, edgecolor='white', label='F1 Score')
+    # Create a new figure for the second plot with transparent background
+    fig, ax = plt.subplots(figsize=(8, 6))
+    fig.patch.set_alpha(0.0)
 
-    plt.xticks([r + barWidth for r in range(len(models))], models)
-    plt.legend()
-    plt.savefig(output_file)
-    plt.show()
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.savefig(output_file, format='png', dpi=300)
-    plt.show()
+    ax.bar(r1, accuracy, color='gray', hatch='////', width=barWidth, edgecolor='white', label='Accuracy')
+    ax.bar(r2, precision, color='gray', hatch='....', width=barWidth, edgecolor='white', label='Precision')
+    ax.bar(r3, recall, color='gray', hatch='xxxx', width=barWidth, edgecolor='white', label='Recall')
+    ax.bar(r4, f1_score, color='gray', hatch='++++', width=barWidth, edgecolor='white', label='F1 Score')
+    ax.set_ylim([0.7, 0.88])
+    ax.set_xticks([r + barWidth for r in range(len(models))])
+    ax.set_xticklabels(models)
+    ax.legend(ncol=4, bbox_to_anchor=(0, -0.15), loc='lower left')
+    
+    # Save the plot as a file
+    fig.savefig(output_file, format='png', dpi=300, bbox_inches="tight", transparent=True)
--- src/sentiment_analysis_epp/models/task_plot.py
+++ src/sentiment_analysis_epp/models/task_plot.py
@@ -6,30 +6,37 @@ from pathlib import Path
 SRC = Path(__file__).resolve().parent.parent
 BLD = SRC / "bld"
 
+from sentiment_analysis_epp.models.plots import plot_sentiment_bar
+
 @pytask.mark.depends_on(SRC / "data_management" / "data.csv")
 @pytask.mark.produces(BLD / "plots" / "sentiment_hist.png")
-def task_sentiment_hist(depends_on, produces):
+def task_sentiment_bar(depends_on, produces):
     # Load the data
     data = pd.read_csv(depends_on, encoding="ISO-8859-1")
 
     # Create the histogram plot
-    # Count the number of sentiments for each class
-    sentiment_counts = data["Sentiment"].value_counts()
-
-    # Set up the figure
-    fig, ax = plt.subplots(figsize=(8, 6))
-
-    # Create the histogram
-    ax.bar(sentiment_counts.index, sentiment_counts.values, color=["#9b59b6", "#3498db", "#95a5a6"])
-
-    # Add labels and titles
-    ax.set_xlabel("Sentiment")
-    ax.set_ylabel("Number of Reviews")
-    ax.set_title("Distribution of Sentiments in Reviews")
-
-    # Save the figure
-    fig.savefig(produces, dpi=300, bbox_inches="tight")
+    plot_sentiment_bar(data, save_path=produces)
 
     return None
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import pytask
+from pathlib import Path
+import datetime
 
+SRC = Path(__file__).resolve().parent.parent
+BLD = SRC / "bld"
+
+from sentiment_analysis_epp.models.plots import plot_performance
+
+@pytask.mark.depends_on(SRC / "bld" / "python" / "evaluation_metrics.csv")
+@pytask.mark.produces(BLD / "plots" / f"performance_plot_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.png")
+def task_performance_plot(depends_on, produces):
+    # Call the plot_performance function
+    plot_performance(input_file=depends_on, output_file=produces)
+
+    # Clear the current figure to avoid overlapping with other plots
+    plt.clf()
+
+    return None
